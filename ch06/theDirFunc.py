994361ed11e04a7735d4dd870b04c9524cce250a
chc1129 <chc1129@gmail.com>
2021-06-01 23:21:19 +0900

    py prg fix

---
+++ ch06/theDirFunc.py
@@ -0,0 +1,17 @@
+import fibo, sys
+print( dir(fibo) )
+print( "---------------------")
+
+print( dir(sys) )
+print( "---------------------")
+
+a = [1, 2, 3, 4, 5]
+import fibo
+fib = fibo.fib
+print( dir() )
+print( "---------------------")
+
+import builtins
+print( dir(builtins) )
+
+
