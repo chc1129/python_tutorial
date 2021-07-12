4660246a7d5fc9b155d2f879c855fb8023f96ce9
chc1129 <chc1129@gmail.com>
2021-07-13 03:06:28 +0900

    python prg fix

---
+++ ch10/randomModule.py
@@ -0,0 +1,6 @@
+import random
+print(random.choice(['apple', 'pear', 'banana']))
+print(random.sample(range(100), 10)) # 重複なしの抽出
+print(random.random()) # ランダムな浮動少数点数
+print(random.randrange(6))
+
---
+++ ch10/stderrWrite.py
@@ -0,0 +1,2 @@
+import sys
+sys.stderr.write('Warning, log file not found tarting a new one\n')
